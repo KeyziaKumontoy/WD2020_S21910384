828f45fc69f9ece53e07ec514dead887194c9cae
keyziakumontoy <keyziakumontoy2001@gmail.com>
2020-11-09 23:14:59 +0700

    exercise 7

---
+++ pertemuan_23/index.js
@@ -0,0 +1,55 @@
+//variabel
+
+//let & cons
+
+// let _umur = 25;
+// _umur = 26;
+// // let nilai = 95.5;
+// // let sudahMenikah = false;
+// // let $nama = "John Doe";
+// // console.log(_umur);
+// // console.log($nama);
+
+//     let alamat;
+//     console.log(alamat);
+
+// // const tahunLahir = 2010;
+// // console.log(tahunLahir);
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1+angka2);
+
+// let umur = 30;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log("Nama Saya" + namaDepan + " " namaBelakang " " + umur " ");
+
+//ternary operator
+ //let angka1 = 5;
+ //let hasil = angka <= 0 ? "Negatif" : "Positif";
+ //console.log(hasil);
+
+// console.log(angka1 <= angka2);
+
+// //operator logika
+// let kondisi1 = 1<2;
+// let kondisi2 = true;
+
+// console.log(kondisi1 || kondisi2);
+
+// console.log(!kondisi1)
+
+//let angka = prompt("Masukkan Angka : ");
+//console.log ("angka yang di masukkan adalah " + angka);
+
+//Exercise 7
+/*
+    Buatlah program Java Script untuk menghitung BMI (Body Mass Index)
+*/
+
+let berat = prompt("Input berat badan anda : kg");
+let tinggi = prompt("Input tinggi badan anda : cm");
+let bmi = berat / (tinggi*tinggi)
+
+console.log ("BMI = " + bmi);
